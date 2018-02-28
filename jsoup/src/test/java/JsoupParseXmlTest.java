c767e90158f5681692c3ba08f4cee3d041e61f0f
ZBHdd <754332450@qq.com>
2018-02-28 21:15:50 +0800

    jsoup

--- jsoup/src/test/java/JsoupParseXmlTest.java
+++ jsoup/src/test/java/JsoupParseXmlTest.java
@@ -1,36 +1,32 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsoupParseXmlTest {
 
-    @BeforeEach
+    @org.junit.jupiter.api.BeforeEach
     void setUp() {
     }
 
-    @AfterEach
+    @org.junit.jupiter.api.AfterEach
     void tearDown() {
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void addNode() {
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void delNode() {
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void findNode() {
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void findClildNodes() {
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void findPnode() {
     }
 }
