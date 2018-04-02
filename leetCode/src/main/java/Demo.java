679ae249ea83f3aed5153a9685916a83456a7a72
hdd <754332450@qq.com>
2018-04-03 00:02:42 +0800

    append a class for TwoSum

--- leetCode/src/main/java/Demo.java
+++ leetCode/src/main/java/Demo.java
@@ -6,34 +6,26 @@ public class Demo {
         for(Animal a : Animal.values()){
             Class<?> server = a.getValue();
             Object object = server.newInstance();
-            Class<?>[] interfaces = server.getInterfaces();
-            Method method = interfaces[0].getMethod(a.getMethod(), Object.class);
+            Method method = server.getMethod(a.getMethod(), Object.class);
             method.invoke(object,a.getName());
             
         }
     }
 }
 
-interface Server{
+interface BaseClient{
     void call(Object s);
 }
 
-interface DogServer extends Server{
-    void call(Object s);
-}
 
-class DogServerimpl implements DogServer{
+class DogClient implements BaseClient{
 
     public void call(Object s) {
         System.out.println(String.format("I am a %s,I can wang wang wang", (String)s));
     }
 }
 
-interface CatServer extends Server{
-    void call(Object s);
-}
-
-class CatServerimpl implements CatServer{
+class CatClient implements BaseClient{
 
     public void call(Object s) {
         System.out.println(String.format("I am a %s,I can miao miao miao", (String)s));
@@ -41,7 +33,7 @@ class CatServerimpl implements CatServer{
 }
 
 enum Animal{
-    DOG("dog","DogServer","call"),CAT("cat","CatServer","call");
+    DOG("dog","DogClient","call"),CAT("cat","CatClient","call");
 
     private String name;
     private String value;
@@ -57,16 +49,12 @@ enum Animal{
         return name;
     }
 
-    public String getMethod() {
-        return method;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Class<?> getValue() throws ClassNotFoundException {
         return Class.forName(value);
+    }
+
+    public String getMethod() {
+        return method;
     }
 
     public void setValue(String value) {
